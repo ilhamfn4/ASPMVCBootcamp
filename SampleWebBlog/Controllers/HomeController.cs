50bcdd8864766463c5d19e6ba7c50cd85aa81639
Lab ITSI-1 <Lab ITSI-1@DESKTOP-GEG5RVK>
2017-07-25 09:18:32 +0700

    Add project files.

---
+++ SampleWebBlog/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Hello(int id, string nama, string alamat)
+        {
+            //test
+            return Content("Hello " + nama + " ID anda " + id.ToString() + " alamat: " + alamat);
+        }
+
+        public ActionResult Registrasi(string nama="",string email="")
+        {
+            List<string> lstNama = new List<string>();
+            lstNama.Add("Erick");
+            lstNama.Add("Budi");
+            lstNama.Add("Alex");
+
+            ViewBag.ListNama = lstNama;
+
+            if (nama == string.Empty)
+                ViewBag.Nama = "Erick Kurniawan";
+            else
+                ViewBag.Nama = nama;
+
+            ViewBag.Email = (email == string.Empty ? "erick@gmail.com" : email);
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FormRegistrasi(string firstname,string lastname,
+            string email)
+        {
+            //return Content(firstname + " " + lastname + " " + email);
+            ViewBag.FullName = firstname + " " + lastname;
+            ViewBag.Email = email;
+            return View();
+        }
+    }
+}
